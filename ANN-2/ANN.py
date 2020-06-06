cdf4f596beb829be9614677f98347f232543df8a
abhipaiangle <abhishekangle6gmail.com>
2020-06-07 02:16:54 +0530

    task done

--- ANN-2/ANN.py
+++ ANN-2/ANN.py
@@ -2,44 +2,43 @@ import json
 
 with open("data.json") as f:
     data=json.load(f)
-
+with open("data_test.json") as g:
+    data_test=json.load(g)
+    
 import numpy as np
 import matplotlib.pyplot as plt
 
-X=[data["input1"],data["input2"]]
-X=np.array(X)
-X=X.reshape(-1,2)
-y=data["flag"]
-y=np.array(y).reshape(-1,1)
+X_train=[data["input1"],data["input2"]]
+X_train=np.array(X_train)
+X_train=X_train.transpose()
+y_train=data["flag"]
+y_train=np.array(y_train).reshape(-1,1)
 
-from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.1,random_state=0)
-'''
-from sklearn.preprocessing import StandardScaler
-scx= StandardScaler()
-X_train=scx.fit_transform(X_train)
-X_test=scx.fit_transform(X_test)'''
+X_test=[data_test["input1"],data_test["input2"]]
+X_test=np.array(X_test)
+X_test=X_test.transpose()
+y_test=data_test["flag"]
+y_test=np.array(y_test).reshape(-1,1)
 
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
 classifier=Sequential()
 #adding firsthidden layer
-classifier.add(Dense(output_dim=2, init='uniform', activation='relu',input_dim=2)) #relu is the curve which NN will follow
-#adding another hidden layer
-#classifier.add(Dense(output_dim=2, init='uniform', activation='relu'))
+classifier.add(Dense(units=20, activation='relu',input_dim=2))
+
+classifier.add(Dense(units=10, activation='relu'))
 #adding the output layer
-classifier.add(Dense(output_dim=1, init='uniform', activation='sigmoid'))
+classifier.add(Dense(units=1, activation='sigmoid'))
 #compiling the layers
 classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])#adam is for stochastic gradient descend
 #fitting the ANN model
-classifier.fit(X_train,y_train,batch_size=None,epochs=400)
+classifier.fit(X_train,y_train,batch_size=None,epochs=100)
 
 y_pred=classifier.predict(X_test)
 y_pred=(y_pred>0.5)
 
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 cm=confusion_matrix(y_test,y_pred)
-
-    
+accuracy=accuracy_score(y_test,y_pred)
+print(accuracy)
